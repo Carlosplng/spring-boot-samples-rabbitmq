92f628e41f2a79ba639f1b66697a21a091c4f039
qianliping <qianliping@wangda.cn>
2017-07-21 11:30:04 +0800

    add file

---
+++ src/test/java/org/carlos/rabbitmq/ObjectTest.java
@@ -0,0 +1,26 @@
+package org.carlos.rabbitmq;
+
+import org.carlos.model.User;
+import org.carlos.rabbit.object.ObjectSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ObjectTest {
+
+	@Autowired
+	private ObjectSender sender;
+
+	@Test
+	public void sendOject() throws Exception {
+		User user=new User();
+		user.setName("neo");
+		user.setPass("123456");
+		sender.send(user);
+	}
+
+}
